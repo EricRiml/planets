e58c0437c2d45f83c0d986c54d3ef35bc78a811b
Eric <rimlspeed@hotmail.com>
2017-09-06 19:00:28 -0600

    Attempts to version control.  Needs PHP added

--- shipBuilder.js
+++ shipBuilder.js
@@ -6,30 +6,51 @@ $(document).ready(function() {
     
     var loadingError = false;
     
+    var getJSONfile = $.get("http://api.planets.nu/static/all", createShipFile);
+    
     // test for shipFile in Local Storage:
     
     
         // if shipFile does NOT exist:
             // show "loading" modal:
-            loadingModal("show");
+            // loadingModal("show");
     
             // get JSON file from Planets.nu if shipFile not found:
 
-            var ajaxGetFile = $.ajax({
-                                    type: "GET",
-                                    timeout: 3000,
-                                    url: "http://api.planets.nu/static/all",
-                                    dataType: "json",
-                                    success: createShipFile,
-                                    error: loadError
-                                    });
+//            $.ajax({
+//                type: "GET",
+//                timeout: 3000,
+//                url: "http://api.planets.nu/static/all",
+//                dataType: "json",
+//                success: createShipFile(data),
+//                error: loadError()
+//                });
 
             // parse JSON file, create needed objects, lists or arrays:
             function createShipFile(data)
             {
-        
+                
+                 var jsonObj = JSON.stringify(data, undefined, 4);
+                console.log(jsonObj);
+                console.log("New File");
+                console.log("New file after remove no cache in HTML");//
+//                if (typeof data == "object")
+//                    {
+//                        var jsonObj = JSON.stringify(data, undefined, 4);
+//                        console.log(jsonObj);
+//                        
+//                    }
+//                else
+//                {
+//                    console.log("Error loading file.");
+//                }
         
             }
+    
+//            function loadError()
+//                {
+//                    console.log("Error loading file.");
+//                }
             // store parsed ship information in Local Storage as JSON objects:
                 // store hulls:
     
@@ -40,7 +61,7 @@ $(document).ready(function() {
                 // store torpedo tubes:
     
             // hide loading modal:
-            loadingModal("hide");
+            // loadingModal("hide");
     
     
     
